eba66e42b4f002ccbfb922c964c97afc6db51366
ABHISHEK SINGH <abhishekk.singhh@gmail.com>
2020-05-06 22:41:17 +0530

    Added thumbnail view, higlight, strikeout, underline and doc save features

--- PDFDocView.swift
+++ PDFDocView.swift
@@ -13,7 +13,7 @@ class PDFDocView {
     var pdfView : PDFView!
     
     init(width: CGFloat, height: CGFloat) {
-        pdfView = PDFView(frame: CGRect(x: 0, y: 60, width: width, height: height))
+        pdfView = PDFView(frame: CGRect(x: 0, y: 60, width: width, height: height - 60))
         //pdfView.autoresizingMask = [.viewWidthSizable, .viewHeightSizable]
         pdfView.autoScales = true
         pdfView.backgroundColor = UIColor.systemGray2
@@ -28,10 +28,75 @@ class PDFDocThumbNailView {
         thumbnailView = PDFThumbnailView()
         thumbnailView.translatesAutoresizingMaskIntoConstraints = false
         
-        thumbnailView.heightAnchor.constraint(equalToConstant: 80).isActive = true
-        thumbnailView.thumbnailSize = CGSize(width: 40, height: 40)
+        thumbnailView.heightAnchor.constraint(equalToConstant: 30).isActive = true
+        thumbnailView.thumbnailSize = CGSize(width: 15, height: 15)
         thumbnailView.layoutMode = .horizontal
    
         thumbnailView.backgroundColor = UIColor.systemGray2.withAlphaComponent(1.0)
     }
 }
+
+class PDFRenderer {
+    let pageRect = CGRect(x: 0, y: 0, width: 595.2, height: 841.8)
+    var pdfRenderer: UIGraphicsPDFRenderer!
+    var outputFileURL: URL!
+    
+    init() {
+        pdfRenderer = UIGraphicsPDFRenderer(bounds: pageRect)
+    }
+    
+}
+
+class Watermark: PDFPage {
+    
+
+//    override func draw(with box: PDFDisplayBox, to context: CGContext) {
+//        // Draw a custom purple line.
+//        UIGraphicsPushContext(context)
+//        context.saveGState()
+//
+//
+//        let path = UIBezierPath()
+//        path.lineWidth = 10
+//        path.move(to: CGPoint(x: bounds.minX + startPoint.x, y: bounds.minY + startPoint.y))
+//        path.addLine(to: CGPoint(x: bounds.minX + endPoint.x, y: bounds.minY + endPoint.y))
+//        UIColor.systemPurple.setStroke()
+//        path.stroke()
+//
+//        context.restoreGState()
+//        UIGraphicsPopContext()
+//    }
+
+    // 3. Override PDFPage custom draw
+    /// - Tag: OverrideDraw
+    override func draw(with box: PDFDisplayBox, to context: CGContext) {
+        print("Watermark draw called")
+
+        // Draw original content
+        super.draw(with: box, to: context)
+
+        // Draw rotated overlay string
+        UIGraphicsPushContext(context)
+        context.saveGState()
+
+        let pageBounds = self.bounds(for: box)
+        context.translateBy(x: 0.0, y: pageBounds.size.height)
+        context.scaleBy(x: 1.0, y: -1.0)
+        context.rotate(by: CGFloat.pi / 4.0)
+
+        let string: NSString = "U s e r   3 1 4 1 5 9"
+        let attributes: [NSAttributedString.Key: Any] = [
+            NSAttributedString.Key.foregroundColor: #colorLiteral(red: 0.4980392157, green: 0.4980392157, blue: 0.4980392157, alpha: 0.5),
+            NSAttributedString.Key.font: UIFont.boldSystemFont(ofSize: 64)
+        ]
+
+        string.draw(at: CGPoint(x: 250, y: 40), withAttributes: attributes)
+
+        context.restoreGState()
+        UIGraphicsPopContext()
+
+    }
+    
+    
+}
+
--- ViewController.swift
+++ ViewController.swift
@@ -9,12 +9,14 @@
 import UIKit
 import PDFKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, PDFDocumentDelegate {
     
     var pdfDocView: PDFDocView!
     var pdfDocThumbNailView: PDFDocThumbNailView!
     var navigationBar: UINavigationBar!
     var naviBarItem: UINavigationItem = UINavigationItem(title: "")
+    var pdfRenderer : PDFRenderer!
+    var fileName : URL!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,14 +25,27 @@ class ViewController: UIViewController {
     }
     
     override func viewWillLayoutSubviews() {
+        setup()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        loadPDFFile()
+    }
+    
+    private func setup(){
         let width = self.view.frame.width
         let height = self.view.frame.height
         navigationBar = UINavigationBar(frame: CGRect(x: self.view.safeAreaInsets.left, y: self.view.safeAreaInsets.top, width: width, height: 54))
         view.addSubview(navigationBar);
 
-        let doneBtn = UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.organize, target: nil, action: #selector(loadPDFFile))
+        let loadFileBtn = UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.organize, target: nil, action: #selector(loadPDFFile))
+        
+        pdfRenderer = PDFRenderer()
+        
+        let renderBtn = UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.action, target: nil, action: #selector(saveFile))
     
-        naviBarItem.leftBarButtonItem = doneBtn
+        naviBarItem.rightBarButtonItem = renderBtn
+        naviBarItem.leftBarButtonItem = loadFileBtn
         navigationBar.setItems([naviBarItem], animated: false)
         //navigationBar.backgroundColor = UIColor.clear
         navigationBar.barStyle = UIBarStyle.black
@@ -47,18 +62,61 @@ class ViewController: UIViewController {
         pdfDocThumbNailView.thumbnailView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor).isActive = true
         pdfDocThumbNailView.thumbnailView.pdfView = pdfDocView.pdfView
         
+        // For MenuItem setup
+        let menuController: UIMenuController = UIMenuController.shared
+        //menuController.setMenuVisible(true, animated: true)
+        let highLightItem = UIMenuItem(title:"Highlight", action: #selector(highlightFromMenuItem))
+        let underlineItem = UIMenuItem(title:"UnderLine", action: #selector(underlineFromMenuItem))
+        let strikeoutItem = UIMenuItem(title:"StrikeOut", action: #selector(strikeoutFromMenuItem))
+        menuController.menuItems = [highLightItem,underlineItem,strikeoutItem]
     }
     
-    override func viewDidAppear(_ animated: Bool) {
-        loadPDFFile()
+    
+    @objc func highlightFromMenuItem() {
+        if let document = self.pdfDocView.pdfView.document  {
+           let pdfSelection : PDFSelection = PDFSelection(document: document)
+           pdfSelection.add((self.pdfDocView.pdfView?.currentSelection)!)
+           let arrayByLines = self.pdfDocView.pdfView?.currentSelection?.selectionsByLine()
+               arrayByLines?.forEach({ (selection) in
+                let annotation = PDFAnnotation(bounds: selection.bounds(for: (self.pdfDocView.pdfView?.currentPage)!), forType: .highlight, withProperties: nil)
+                   annotation.color = .yellow
+                   self.pdfDocView.pdfView?.currentPage?.addAnnotation(annotation)
+               })
+            }
+    }
+    @objc func underlineFromMenuItem() {
+        if let document = self.pdfDocView.pdfView.document  {
+           let pdfSelection : PDFSelection = PDFSelection(document: document)
+           pdfSelection.add((self.pdfDocView.pdfView?.currentSelection)!)
+           let arrayByLines = self.pdfDocView.pdfView?.currentSelection?.selectionsByLine()
+               arrayByLines?.forEach({ (selection) in
+                let annotation = PDFAnnotation(bounds: selection.bounds(for: (self.pdfDocView.pdfView?.currentPage)!), forType: .underline, withProperties: nil)
+                   annotation.color = .green
+                   self.pdfDocView.pdfView?.currentPage?.addAnnotation(annotation)
+               })
+            }
+    }
+    @objc func strikeoutFromMenuItem() {
+        if let document = self.pdfDocView.pdfView.document  {
+           let pdfSelection : PDFSelection = PDFSelection(document: document)
+           pdfSelection.add((self.pdfDocView.pdfView?.currentSelection)!)
+           let arrayByLines = self.pdfDocView.pdfView?.currentSelection?.selectionsByLine()
+               arrayByLines?.forEach({ (selection) in
+                let annotation = PDFAnnotation(bounds: selection.bounds(for: (self.pdfDocView.pdfView?.currentPage)!), forType: .strikeOut, withProperties: nil)
+                   annotation.color = .blue
+                   self.pdfDocView.pdfView?.currentPage?.addAnnotation(annotation)
+               })
+            }
     }
     
     @objc func loadPDFFile(){
-        
         let picker = DocumentPickerViewController(
             supportedTypes: ["com.adobe.pdf"],//["public.text"],
             onPick: { url in
                 print("url : \(url)")
+                self.pdfRenderer.outputFileURL = url
+                self.fileName = url
+                
                 //var filePath : String = "/name.pdf"
                 let filePath = url.absoluteString
                 let name = filePath.split(separator: "/")
@@ -67,7 +125,10 @@ class ViewController: UIViewController {
                 //print(name[count-1])
                 self.navigationItem.title = String(name[count - 1])
                 if let document = PDFDocument(url: url){
+                    //document.delegate = self
                     self.pdfDocView.pdfView.document = document
+                    //self.pdfDocView.pdfView.documentView?.backgroundColor = UIColor.red
+                    
                 }
                 
             },
@@ -79,6 +140,16 @@ class ViewController: UIViewController {
         //picker.modalPresentationStyle = .
         picker.modalTransitionStyle = .coverVertical
         UIApplication.shared.windows.first?.rootViewController?.present(picker, animated: true)
+    }
+    
+    @objc func saveFile(){
+        self.pdfDocView.pdfView.document?.write(to: self.fileName)
+    }
+    
+    func classForPage() -> AnyClass {
+        
+        return Watermark.self
+        
     }
 
 }
